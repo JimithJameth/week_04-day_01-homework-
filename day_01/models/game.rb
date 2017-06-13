f72f2bc6e5787381bdfd46c1a558353fcf6d350f
Jamie Martin <jamiemartinmusic@gmail.com>
2017-06-13 12:41:55 +0100

    updated rock paper and scissors

---
+++ day_01/models/game.rb
@@ -0,0 +1,25 @@
+class Game
+
+def initialize(player_1,player_2)
+  @player_1 = player_1
+  @player_2 = player_2
+end
+
+def run()
+
+  rules ={
+    'rock' =>'paper',
+    'paper' => 'scissors',
+    'scissors' => 'rock'
+  }
+
+  if @player_1 == rules[@player_2]
+    return @player_1 + " wins!"
+  end
+
+  if @player_2 == rules[@player_1]
+    return @player_2 + " wins!"
+  end
+  return "Draw!"
+end
+end
